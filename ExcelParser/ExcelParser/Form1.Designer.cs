f04ff15fcbbeb97614bc6f357a02495d19daa05e
Kazhin Philipp <kazhin.philipp@gmail.com>
2017-03-20 23:53:29 +0500

    Добавил кнопки

--- ExcelParser/ExcelParser/Form1.Designer.cs
+++ ExcelParser/ExcelParser/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.button_AddFile = new System.Windows.Forms.Button();
             this.button_EditFile = new System.Windows.Forms.Button();
             this.button_DeleteFile = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox_Files
@@ -70,11 +72,31 @@
             this.button_DeleteFile.Text = "Удалить";
             this.button_DeleteFile.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 36);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Соеденить файлы выделенных сотрудников";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 36);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Соеденить файлы всех сотрудников";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 240);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button_DeleteFile);
             this.Controls.Add(this.button_EditFile);
             this.Controls.Add(this.button_AddFile);
@@ -92,6 +114,8 @@
         private System.Windows.Forms.Button button_AddFile;
         private System.Windows.Forms.Button button_EditFile;
         private System.Windows.Forms.Button button_DeleteFile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
